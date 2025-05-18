5060dd97c29804c48e3647c963d0f30ec337d5ce
Magali Bechis <magalibechis3@gmail.com>
2025-05-18 11:51:50 -0300

    Menú principal procesos.sh

--- procesos.sh
+++ procesos.sh
@@ -6,3 +6,20 @@ validar_pid() {
         return 1
     fi
 }
+# Menú de opciones para gestión de procesos
+while true; do
+    clear
+    echo "=== Gestión de Procesos ==="
+    echo "1. Ver procesos activos"
+    echo "2. Ver información de un proceso específico"
+    echo "3. Terminar un proceso con SIGTERM"
+    echo "4. Volver al menú principal"
+    echo -n "Seleccione una opción: "
+    read opción
+
+    case $opcion in
+
+	# CONTENIDO DEL SH
+
+    esac
+done
